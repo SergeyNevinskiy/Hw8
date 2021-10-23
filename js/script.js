25a0024282ff23834bba1e3e7150fd148edf976e
SergeyNevinskiy <88575089+SergeyNevinskiy@users.noreply.github.com>
2021-10-23 23:18:54 +0300

    Hw8

---
+++ js/script.js
@@ -0,0 +1,91 @@
+// 1
+// const a = +prompt("Введите число");
+// a === 0 ? alert("Верно") : alert("Неверно");
+
+// 2
+// const a = +prompt("Введите число");
+// a > 0 ? alert("Верно") : alert("Неверно");
+
+// 3
+// const a = +prompt("Введите число");
+// a < 0 ? alert("Верно") : alert("Неверно");
+
+// 4
+// const a = +prompt("Введите число");
+// a >= 0 ? alert("Верно") : alert("Неверно");
+
+// 5
+// const a = +prompt("Введите число");
+// a <= 0 ? alert("Верно") : alert("Неверно");
+
+// 6
+// const a = +prompt("Введите число");
+// a !== 0 ? alert("Верно") : alert("Неверно");
+
+// 7
+// const a = prompt("Введите число");
+// a === "test" ? alert("Верно") : alert("Неверно");
+
+// 8
+// const a = +prompt("Введите число");
+// a == "1" ? alert("Верно") : alert("Неверно");
+
+// 9
+// const a = +prompt("Введите число");
+// a > 0 && a < 5 ? alert("Верно") : alert("Неверно");
+
+// 10
+// let a = +prompt("Введите число");
+// if (a === 0 || a === 2) {
+// 	a += 7;
+// } else {
+// 	a /= 10;
+// }
+// alert(a);
+
+// 11
+// const a = +prompt("Введите число");
+// const b = +prompt("Введите число");
+// if (a <= 1 && b >= 3) {
+// 	const sum = a + b;
+// 	alert(sum)
+// } else {
+// 	const sub = a - b;
+// 	alert(sub);
+// }
+
+// 12
+// const a = +prompt("Введите число");
+// const b = +prompt("Введите число");
+// if ((a > 2 && a < 11) || (b >= 6 && B < 14)) {
+// 	alert("Верно")
+// }else {
+// 	alert("Неверно")
+// }
+
+// 13
+// const num = +prompt("Введите число от 1 до 4");
+// let result;
+// switch (num){
+// 	case 1:
+// 		result = 'Зима';
+// 		console.log (result);
+// 		break;
+// 	case 2:
+// 		result = 'Весна';
+// 		console.log (result);
+// 		break;
+// 	case 3:
+// 		result = 'Лето';
+// 		console.log (result);
+// 		break;
+// 	case 4:
+// 		result = 'Осень';
+// 		console.log (result);
+// 		break;
+// 	default:
+		// alert("Вы ввели неверно");
+	
+// }
+
+
